057e17ec52470b1172c28150e84310744b146ebe
Denis.Kitrish <denis.kitrish@outlook.com>
2025-03-04 20:08:10 +0300

    Схема таблицы Post [#1731].

---
+++ src/main/resources/db/liquibase/changelogs/changesets/db-grabber/schema-grabber/table-posts/001_posts_ddl_create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE grabber.posts(
+	id BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
+	"name" TEXT,
+	"text" TEXT,
+	link TEXT,
+	created_at TIMESTAMP,
+	CONSTRAINT posts_pk PRIMARY KEY(id)
+);
+
+CREATE UNIQUE INDEX posts_name_uidx ON grabber.posts("name");
